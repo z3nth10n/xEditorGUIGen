21faf54bfcc31c0710dd41b2563e6b5cb2acc14d
Ikillnukes <ikillnukes1@gmail.com>
2018-10-28 21:39:15 +0100

    Implemeted Layout system + covered some cases

--- xEditorGUIGen/Program.cs
+++ xEditorGUIGen/Program.cs
@@ -15,7 +15,17 @@ namespace xEditorGUIGen
     {
         private static string[] styleNames = new[] { "boldFont", "boldLabel", "centeredGreyMiniLabel", "colorField", "foldout", "foldoutPreDrop", "helpBox", "inspectorDefaultMargins", "inspectorFullWidthMargins", "label", "largeLabel", "layerMaskField", "miniBoldFont", "miniBoldLabel", "miniButton", "miniButtonLeft", "miniButtonMid", "miniButtonRight", "miniFont", "miniLabel", "miniTextField", "numberField", "objectField", "objectFieldMiniThumb", "objectFieldThumb", "popup", "radioButton", "standardFont", "textArea", "textField", "toggle", "toggleGroup", "toolbar", "toolbarButton", "toolbarDropDown", "toolbarPopup", "toolbarTextField", "whiteBoldLabel", "whiteLabel", "whiteLargeLabel", "whiteMiniLabel", "wordWrappedLabel", "wordWrappedMiniLabel" };
 
-        private static string[] methodExceptions = new[] { "DrawTextureTransparent" };
+        private static string[] methodExceptions = new[] { "DrawTextureTransparent", "GetControlRect", "BeginFadeGroup", "BeginScrollView", "BeginVertical", "BeginHorizontal", "BeginToggleGroup", "InspectorTitlebar", "EnumFlagsField", "BeginProperty", "GetSelectedValueForControl", "HandlePrefixLabel" };
+
+        private static string[] unsupportedParams = new[] { "wide", "includeObsolete", "checkEnabled", "labelPosition", "colorWriteMask", "followingStyle" };
+
+        private static string[] typesMismatch = new[] { "Popup", "IntPopup" };
+
+        private static string[] methodsDontNeedPosition = new[] { "IndentedRect", "GetPropertyHeight", "CanCacheInspectorGUI", "PrefixLabel", "FocusTextInControl", "BeginDisabledGroup" };
+
+        private static string[] forcedLayoutMethods = new[] { "PrefixLabel" };
+
+        private static string[] convertedNeeded = new[] { "PrefixLabel" };
 
         private static void Main(string[] args)
         {
@@ -98,10 +108,14 @@ namespace xEditorGUIGen
 
             sb.AppendLine();
 
-            sb.AppendLineIndented("public class xEditorGUI", 1);
+            sb.AppendLineIndented("// I suggest you not to implement anything here due to generation. Use partial approach.", 1);
+            sb.AppendLineIndented("public partial class xEditorGUI", 1);
             sb.AppendLineIndented("{", 1);
 
             sb.AppendLineIndented("private Dictionary<string, StyleWrapper> stylesDict = new Dictionary<string, StyleWrapper>();", 2);
+            sb.AppendLine();
+
+            sb.AppendLineIndented("private Rect _position;", 2);
             sb.AppendLine();
 
             foreach (Match ItemMatch in matches)
@@ -131,14 +145,16 @@ namespace xEditorGUIGen
 
                     bool isSupported = true;
 
-                    if (hasAttributedParams)
-                        foreach (string exceptionName in methodExceptions)
-                            if (methodWithType[1].Contains(exceptionName))
-                            {
-                                if (!hasOptionalAttributedParams)
-                                    sb.AppendLineIndented(@"throw new Exception(""This method isn't supported!"");", 3);
-                                isSupported = false;
-                            }
+                    // Console.WriteLine(methodWithType[1]);
+
+                    if (!hasAttributedParams)
+                        if (methodExceptions.Contains(methodWithType[1]))
+                        {
+                            if (!hasOptionalAttributedParams)
+                                sb.AppendLineIndented(@"throw new Exception(""This method isn't supported!"");", 3);
+
+                            isSupported = false;
+                        }
 
                     if (Params.Contains('='))
                         Params = new Regex(" =.+?,").Replace(Params, ",");
@@ -165,49 +181,87 @@ namespace xEditorGUIGen
                         isEditorMethod = true;
                     }
 
-                    bool hasNoPosition = !parsObjects.Any(x => x.type == "Rect" && x.name == "position");
-
-                    IEnumerable<string> parsNames = parsObjects.Select(x => x.name);
-
-                    string contentParam = parsNames.GetParam();
-
-                    bool hasStyle = parsNames.Contains("style");
-
-                    string pars = parsObjects.GetParams();
-
-                    string styleName = methodWithType[1].FirstCharToLower();
-
-                    bool styleExists = styleNames.Contains(styleName),
-                         addBody = hasStyle || styleExists,
-                         hasNoContent = string.IsNullOrEmpty(contentParam);
-
-                    if (hasNoContent)
-                        contentParam = "GUIContent.none";
-
-                    if (hasNoPosition && isSupported && !isEditorMethod && !hasOptionalAttributedParams)
+                    if (isSupported)
                     {
-                        if (isSupported)
-                            sb.AppendLineIndented(@"throw new Exception(""No implementation Layout yet!"");", 3);
-                    }
+                        bool isLayout = !parsObjects.Any(x => x.type == "Rect" && x.name == "position");
 
-                    if (!isSupported || isEditorMethod || hasOptionalAttributedParams || hasNoPosition)
-                        sb.AppendLineIndented("/*", 3);
+                        IEnumerable<string> parsNames = parsObjects.Select(x => x.name);
 
-                    if (addBody)
+                        string contentParam = parsNames.GetGUIContentParamName();
+
+                        bool hasStyle = parsNames.Contains("style");
+
+                        string pars = parsObjects.GetParams();
+
+                        string styleName = methodWithType[1].FirstCharToLower();
+
+                        bool styleExists = styleNames.Contains(styleName),
+                             // addBody = hasStyle || styleExists,
+                             hasNoContent = string.IsNullOrEmpty(contentParam);
+
+                        if (hasNoContent)
+                            contentParam = "GUIContent.none";
+
+                        if (isLayout && !isEditorMethod && !hasOptionalAttributedParams && isSupported)
+                            sb.AppendLineIndented(@"// throw new Exception(""No implementation Layout yet!"");", 3);
+
+                        if (parsObjects.Any(x => x.name == "options"))
+                        {
+                            sb.AppendLineIndented(@"
+                                                if (options != null)
+                                                    throw new ArgumentException(""Options use isn't supported yet!"", ""options"");
+                                    ", 3);
+                        }
+
+                        if (isEditorMethod || hasOptionalAttributedParams)
+                            sb.AppendLineIndented("/*", 3);
+
                         sb.AppendLineIndented($@"if(!stylesDict.ContainsKey(""{methodWithType[1]}""))", 3);
 
-                    if (hasStyle)
-                        sb.AppendLineIndented($@"stylesDict.Add(""{methodWithType[1]}"", new StyleWrapper(style, {contentParam}));", 4);
-                    else if (styleExists)
-                        sb.AppendLineIndented($@"stylesDict.Add(""{methodWithType[1]}"", new StyleWrapper(EditorStyles.{styleName}, {contentParam}));", 4);
+                        string currentStyle = hasStyle ? "style" : styleExists ? $"EditorStyles.{styleName}" : $@"new GUIStyle(""{styleName}"")";
 
-                    if (addBody)
+                        sb.AppendLineIndented($@"stylesDict.Add(""{methodWithType[1]}"", new StyleWrapper({currentStyle}, {contentParam}));", 4);
                         sb.AppendLine();
 
-                    sb.AppendLineIndented($"{(methodWithType[0] == "void" ? "" : "return ")}EditorGUI.{methodWithType[1]}({pars});", 3);
+                        bool dontNeedPosition = methodsDontNeedPosition.Contains(methodWithType[1]); // (!isLayout || isLayout);
 
-                    if (!isSupported || isEditorMethod || hasOptionalAttributedParams || hasNoPosition)
-                        sb.AppendLineIndented("*/", 3);
+                        if (forcedLayoutMethods.Contains(methodWithType[1]) && !parsObjects.Any(x => x.type == "Rect"))
+                            dontNeedPosition = false;
+
+                        if (!dontNeedPosition)
+                        {
+                            sb.AppendLineIndented($"UpdatePosition({currentStyle}, {contentParam});", 3);
+                            sb.AppendLine();
+                        }
+
+                        string _unsupportedParams = "",
+                               modifiedParams = F.GetLayoutParams(pars, unsupportedParams, isLayout && !dontNeedPosition, out _unsupportedParams);
+
+                        if (!string.IsNullOrEmpty(_unsupportedParams))
+                            sb.AppendLineIndented($"// This call has the following ({_unsupportedParams.Count(x => x == ',') + 1}) unsupported params (an implementation is required): {_unsupportedParams}", 3);
+
+                        // parsObjects.Any(x => x.type == "string[]") &&
+                        if (parsObjects.Any(x => x.type.Contains("GUIContent")) && typesMismatch.Contains(methodWithType[1]))
+                        {
+                            Param param = parsObjects.First(x => x.type == "string[]");
+
+                            modifiedParams = modifiedParams.Replace(param.name, param.name + ".Select(x => new GUIContent(x)).ToArray()");
+                        }
+
+                        if (convertedNeeded.Contains(methodWithType[1]) && parsObjects.First(x => x.name == "label").type == "string")
+                            modifiedParams = new Regex(@"\blabel\b").Replace(modifiedParams, "(Converters)label");
+
+                        if (methodWithType[1] == "HelpBox" && !parsObjects.Any(x => x.type == "MessageType"))
+                        {
+                            modifiedParams = modifiedParams.Replace("content", "content.text");
+                            modifiedParams += ", default(MessageType)";
+                        }
+
+                        sb.AppendLineIndented($"{(methodWithType[0] == "void" ? "" : "return ")}EditorGUI.{methodWithType[1]}({modifiedParams});", 3);
+
+                        if (isEditorMethod || hasOptionalAttributedParams)
+                            sb.AppendLineIndented("*/", 3);
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -230,9 +284,37 @@ namespace xEditorGUIGen
                                             size += wrapper.listWrapper.Sum(x => x.Value.list.Sum(y => x.Key.CalcSize(y).y));
 
                                         return size;
+                                    }
+
+                                    private void UpdatePosition(GUIStyle style, string label)
+                                    {
+                                        UpdatePosition(style, new GUIContent(label));
+                                    }
+
+                                    private void UpdatePosition(GUIStyle style, GUIContent content)
+                                    {
+                                        Vector2 vector = style.CalcSize(content);
+
+                                        float width = 0, height = 0;
+
+                                        style.CalcMinMaxWidth(content, out width, out height);
+
+                                        _position.yMin += vector.y;
+                                        _position.xMin = vector.y;
+                                        _position.width = width;
+                                        _position.height = height;
                                     }", 2);
 
             sb.AppendLineIndented("}", 1);
+
+            sb.AppendLineIndented(@"
+                                    public class Converters : GUIContent
+                                    {
+                                        public static implicit operator Converters(string str)
+                                        {
+                                            return (Converters)new GUIContent(str);
+                                        }
+                                    }", 1);
 
             sb.AppendLine("}");
 
@@ -291,7 +373,7 @@ namespace xEditorGUIGen
             thread.Join();
         }
 
-        public static string GetParam(this IEnumerable<string> pars)
+        public static string GetGUIContentParamName(this IEnumerable<string> pars)
         {
             if (pars.Contains("label"))
                 return "label";
@@ -375,6 +457,45 @@ namespace xEditorGUIGen
                 //Stack trace is not available!
             }
             return linenum;
+        }
+
+        public static string GetLayoutParams(string pars, string[] unsupported, bool isLayout, out string unsupportedParams)
+        {
+            List<string> uPars = new List<string>();
+
+            bool hasUnsupportedParams = false;
+
+            foreach (string unsupport in unsupported)
+                if (pars.Contains(unsupport))
+                {
+                    hasUnsupportedParams = true;
+                    uPars.Add(unsupport);
+                }
+
+            if (hasUnsupportedParams)
+            {
+                unsupported.ToList().ForEach(o => pars = pars.Replace(o, string.Empty));
+                pars = pars.ReplaceRecursive(", ,", ",");
+                pars = new Regex(", $").Replace(pars, "");
+            }
+
+            unsupportedParams = string.Join(", ", uPars);
+
+            if (isLayout)
+                return "_position, " + pars.Replace(", options", "");
+            else
+                return pars;
+        }
+
+        public static string ReplaceRecursive(this string str, string find, string replace)
+        {
+            if (str.Contains(find))
+            {
+                str = str.Replace(find, replace);
+                str = ReplaceRecursive(str, find, replace);
+            }
+
+            return str;
         }
     }
 
